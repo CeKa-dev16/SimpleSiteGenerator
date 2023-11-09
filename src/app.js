5c05f17dcf26f43ae46900e1123874de067d6f98
Nazareno Soto <nazarenosoto75@gmail.com>
2023-11-09 18:38:32 -0300

    Refactored canvas events, styles to copy button

--- src/app.js
+++ src/app.js
@@ -6,4 +6,26 @@ function setCanvasTo(element) {
   canvas = element;
 }
 
-export { setCanvasTo, main, original_canvas, canvas };
+let hovered = canvas;
+
+const handle_mousemove = (event) => {
+  hovered.style.outline = '';
+  hovered = document.elementFromPoint(event.x, event.y);
+  hovered.style.outline = '2px dashed var(--body)';
+}
+
+const handle_mouseleave = () => {
+  hovered.style.outline = '';
+}
+
+function activateEvents() {
+  canvas.addEventListener('mousemove', handle_mousemove);
+  canvas.addEventListener('mouseleave', handle_mouseleave);
+}
+
+function desactivateEvents() {
+  canvas.removeEventListener('mousemove', handle_mousemove);
+  canvas.removeEventListener('mouseleave', handle_mouseleave);
+}
+
+export { setCanvasTo, main, original_canvas, canvas, activateEvents, desactivateEvents };
