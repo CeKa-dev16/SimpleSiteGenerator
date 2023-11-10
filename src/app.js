be8cc634912a8805404d2a778d64ba7fff5a84cb
Nazareno Soto <nazarenosoto75@gmail.com>
2023-11-10 19:02:13 -0300

    'Copy Code' -> 'Copy Image'

--- src/app.js
+++ src/app.js
@@ -1,31 +1,38 @@
-const main = document.querySelector('main');
-const original_canvas = document.querySelector('.canvas').firstChild;
-var canvas = document.querySelector('#canvas');
+const main = document.querySelector("main");
+const original_canvas = document.querySelector(".canvas").firstChild;
+var ownCanvas = document.querySelector("#canvas");
 
 function setCanvasTo(element) {
-  canvas = element;
+  ownCanvas = element;
 }
 
 let hovered = canvas;
 
 const handle_mousemove = (event) => {
-  hovered.style.outline = '';
+  hovered.style.outline = "";
   hovered = document.elementFromPoint(event.x, event.y);
-  hovered.style.outline = '2px dashed var(--body)';
-}
+  hovered.style.outline = "2px dashed var(--body)";
+};
 
 const handle_mouseleave = () => {
-  hovered.style.outline = '';
-}
+  hovered.style.outline = "";
+};
 
 function activateEvents() {
-  canvas.addEventListener('mousemove', handle_mousemove);
-  canvas.addEventListener('mouseleave', handle_mouseleave);
+  ownCanvas.addEventListener("mousemove", handle_mousemove);
+  ownCanvas.addEventListener("mouseleave", handle_mouseleave);
 }
 
 function desactivateEvents() {
-  canvas.removeEventListener('mousemove', handle_mousemove);
-  canvas.removeEventListener('mouseleave', handle_mouseleave);
+  ownCanvas.removeEventListener("mousemove", handle_mousemove);
+  ownCanvas.removeEventListener("mouseleave", handle_mouseleave);
 }
 
-export { setCanvasTo, main, original_canvas, canvas, activateEvents, desactivateEvents };
+export {
+  setCanvasTo,
+  main,
+  original_canvas,
+  ownCanvas,
+  activateEvents,
+  desactivateEvents,
+};
